8bef803940efe93ad66da5c59b9a18af77f30431
gurn1 <luke@lscwebdesign.co.uk>
2025-07-18 15:05:36 +0100

    Created calculate functionality

--- includes/TimeToReadCalculate.php
+++ includes/TimeToReadCalculate.php
@@ -19,14 +19,32 @@ if( ! class_exists('TimeToReadCalculate') ) {
      * 
      * @since 1.0.0
      */
-	protected static $_instance = null;
+	  protected static $_instance = null;
+
+    /**
+     * Post id
+     * 
+     * @since 1.0.0
+     * @return int
+     */
+    protected static $post_id = 0;
+
+    /**
+     * Words per minute
+     * 
+     * @since 1.0.0
+     * @return int
+     */
+    protected static $words_per_minute = 0;
 
     /**
      * Constructor
      * 
      * @since 1.0.0
      */
-    public function __construct() {
+    public function __construct($post_id,  $wpm = 200 ) {
+      self::$post_id = $post_id;
+      self::$words_per_minute = $wpm;
     }
 
     /**
@@ -36,11 +54,52 @@ if( ! class_exists('TimeToReadCalculate') ) {
      *
      * @since 1.0.0
      */
-    public static function instance() {
+    public static function instance($post_id, $wpm = 200) {
       if ( is_null( self::$_instance ) ) {
-        self::$_instance = new self();
+        self::$_instance = new self($post_id, $wpm);
       }
       return self::$_instance;
+    }
+
+    /**
+     * Get the content
+     * 
+     * @since 1.0.0
+     * @return string
+     */
+    public function get_the_content() {
+      $post = get_post(self::$post_id);
+
+      if( !$post ) {
+        return;
+      }
+
+      return property_exists($post, 'post_content') ? $post->post_content : '';
+    }
+
+    /**
+     *  Calculate the reading time
+     * 
+     * @since 1.0.0
+     * @return int|WP_Error
+     */
+    public function get_reading_time() {
+      $content = $this->get_the_content();
+
+      if( !$content ) {
+        return 0;
+      }
+
+      $trimmed_text = trim( wp_strip_all_tags( strip_shortcodes($content) ) );
+      
+      if( empty($trimmed_text) ) {
+        return 0;
+      }
+
+      $word_count = str_word_count( $trimmed_text );
+      $minutes = round( $word_count / self::$words_per_minute, 1 );
+
+      return $minutes;
     }
     
   }
